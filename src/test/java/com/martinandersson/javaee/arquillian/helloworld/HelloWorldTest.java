d88a3c18e68b579eceb1136887af2d3997c867ce
Martin Andersson <webmaster@martinandersson.com>
2014-07-06 16:21:36 +0200

    Maven EJB plugin bug workaround.

--- src/test/java/com/martinandersson/javaee/arquillian/helloworld/HelloWorldTest.java
+++ src/test/java/com/martinandersson/javaee/arquillian/helloworld/HelloWorldTest.java
@@ -6,7 +6,6 @@ import javax.inject.Inject;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.After;
 import org.junit.AfterClass;
