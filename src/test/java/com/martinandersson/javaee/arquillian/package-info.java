f2b35a9262c10f88003f3e33f160c139eb84d9fb
unknown <p8andem2@SSEP8MW70048.slgd.local>
2014-08-19 18:56:07 +0200

    JavaDoc improvement

--- src/test/java/com/martinandersson/javaee/arquillian/package-info.java
+++ src/test/java/com/martinandersson/javaee/arquillian/package-info.java
@@ -1,13 +1,17 @@
 /**
- * Introduces Arquillian and the project setup. Subpackages goes into depth of
- * Arquillian and at the same time, introduce core Java EE concepts such as
- * servlets, EJB:s and JPA.<p>
+ * Introduces Arquillian and the project setup.<p>
+ * 
+ * Subpackages goes into depth of Arquillian and at the same time, introduce
+ * core Java EE concepts such as servlets, EJB:s and JPA.<p>
  * 
  * Begin your study in the "helloworld"-subpackage, then move to the more
  * advanced packages "clientserver" and "persistence". Always read the
  * "package-info.java" file first.<p>
  * 
- * Packages that dig into the details of Java EE concepts will be placed in
- * "com.martinandersson.javaee.**concept**".
+ * Packages that illustrate and test specific Java EE concepts will be placed
+ * in:
+ * <pre>{@code
+ *     com.martinandersson.javaee.**concept**
+ * }</pre>
  */
 package com.martinandersson.javaee.arquillian;
