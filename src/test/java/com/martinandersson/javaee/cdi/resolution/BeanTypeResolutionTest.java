61b798034d850037a3fddec6781b38cc9bcb1312
unknown <p8andem2@SSEP8MW70048.slgd.local>
2014-09-24 16:42:13 +0200

    Mini improvements

--- src/test/java/com/martinandersson/javaee/cdi/resolution/BeanTypeResolutionTest.java
+++ src/test/java/com/martinandersson/javaee/cdi/resolution/BeanTypeResolutionTest.java
@@ -3,7 +3,6 @@ package com.martinandersson.javaee.cdi.resolution;
 import com.martinandersson.javaee.utils.Deployments;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
