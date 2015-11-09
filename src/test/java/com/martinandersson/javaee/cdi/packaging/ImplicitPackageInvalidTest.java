2de1acc144b439788fa0ed75c26462b7a15dcb8c
unknown <p8andem2@SUSD4M-700586.swisslog.net>
2015-11-09 11:32:49 -0500

    Improved docs.

--- src/test/java/com/martinandersson/javaee/cdi/packaging/ImplicitPackageInvalidTest.java
+++ src/test/java/com/martinandersson/javaee/cdi/packaging/ImplicitPackageInvalidTest.java
@@ -38,7 +38,7 @@ import org.junit.runner.RunWith;
  *       subclass of javax.enterprise.inject.spi.DeploymentException."
  * }</pre>
  * 
- * So the specification is pritty clear. The deployment is not supposed to
+ * So the specification is pretty clear. The deployment is not supposed to
  * happen.<p>
  * 
  * 
@@ -46,16 +46,16 @@ import org.junit.runner.RunWith;
  * <h3>The Arquillian @ShouldThrowException dilemma</h3>
  * 
  * Inspecting the server logs after running this test reveal that both WildFly
- * 8.1.0 and GlassFish 4.1 throw this exception (WildFly 8.2.0 won't pass this
- * test):
+ * 8.1.0 and GlassFish 4.1 throw this exception (WildFly 8.2.0 and 9.0.0.CR2
+ * won't pass this test):
  * 
  * <pre>{@code
  *     org.jboss.weld.exceptions.DeploymentException.class
  * }</pre>
  * 
  * ..which extend {@code javax.enterprise.inject.spi.DeploymentException} so
- * this behavior are in full accordance with the specification. Therefore, it
- * would be great if we could specify the expected exception class in the value
+ * this behavior are in compliance with the specification. Therefore, it would
+ * be great if we could specify the expected exception class in the value
  * attribute of {@code @ShouldThrowException}. However, doing so make the
  * client, that is Arquillian, go crazy, throwing:
  * <pre>{@code
@@ -80,7 +80,9 @@ import org.junit.runner.RunWith;
  * 
  * <h3>Results</h3>
  * 
- * Work on GlassFish 4.1 and WildFly 8.1.0, but not WildFly 8.2.0.<p>
+ * Work on GlassFish 4.1 and WildFly 8.1.0, but not WildFly 8.2.0 and 9.0.0.CR2.
+ * The problem seems to be that later WildFly versions do not discover
+ * non-public EJB classes?<p>
  * 
  * TODO: Report a bug.
  * 
@@ -101,24 +103,26 @@ public class ImplicitPackageInvalidTest
     }
     
     @Test // <-- implicitly @RunAsClient given that @ShouldThrowException is put on @Deployment
-    public void implicitBeanArchiveRequireExplicitBeanAnnotations() {
+    public void weHaveToProvideAtLeastOneTestCase() {
         // Empty
     }
 }
 
 @Singleton
 @Startup
-class ForceBeanDiscovery {
-    @Inject CalculatorUnAnnotated unAnnotated;
+class ForceBeanDiscovery
+{
+    @Inject
+    CalculatorUnAnnotated unAnnotated;
     
     @PostConstruct
     public void helloWorld() {
         /*
          * GlassFish 4.1 and WildFly 8.1.0 pass this test even without the
-         * @Startup annotation or this life cycle callback. WildFly 8.2.0 deploy
-         * the archive but won't log this message. Apparently, WildFly 8.2.0
-         * won't discover the ForceBeanDiscovery bean. As far as I can tell,
-         * EJB 3.2 doesn't require an EJB to have public access.
+         * @Startup annotation and this life cycle callback. WildFly 8.2.0 and
+         * 9.0.0.CR2 deploy the archive but won't log this message. Apparently
+         * WildFly won't discover the ForceBeanDiscovery bean. As far as I can
+         * tell, EJB 3.2 doesn't require an EJB class to have public access.
          */
         Logger.getLogger(ForceBeanDiscovery.class.getName()).severe("Not supposed to be deployed!");
     }
