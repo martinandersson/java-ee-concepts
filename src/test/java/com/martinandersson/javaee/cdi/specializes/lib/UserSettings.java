7fda9b6125b7545534d8d03528da89e9ef76d9b1
unknown <p8andem2@SUSD4M-700586.swisslog.net>
2016-04-22 10:55:00 -0400

    Removed redundant access modifier.

--- src/test/java/com/martinandersson/javaee/cdi/specializes/lib/UserSettings.java
+++ src/test/java/com/martinandersson/javaee/cdi/specializes/lib/UserSettings.java
@@ -4,5 +4,5 @@ package com.martinandersson.javaee.cdi.specializes.lib;
  * @author Martin Andersson (webmaster at martinandersson.com)
  */
 public interface UserSettings {
-    public boolean allowMultipleDevices();
+    boolean allowMultipleDevices();
 }
